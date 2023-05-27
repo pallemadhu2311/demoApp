6d288a2d84d2858fad54e20e11600ee17d2a27cf
Palle Madhu <madhupalle2311@gmail.com>
2023-05-27 19:58:46 +0530

    Added Changes : Created SignUp Frontend Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,13 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
@@ -35,7 +42,13 @@ import {MatListModule} from '@angular/material/list';
     MatSidenavModule,
     MatButtonModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatCardModule
 
   ],
   providers: [],
